8a6ec7e8d42dc0f118fd65bff5d07fc7e7e0baf7
KilianINFO2 <cornec.e1801357@etud.univ-ubs.fr>
2021-04-30 14:20:05 +0200

    Aélioration precicision

--- main.py
+++ main.py
@@ -25,19 +25,19 @@ numberPattern = "^.*[0-9].*$"
 numberMatcher = re.compile(numberPattern)
 abstractPattern = "^.*[Aa][Bb][Ss][Tt][Rr][Aa][Cc][Tt].*$"
 abstractMatcher = re.compile(abstractPattern)
-introPattern = "^.*[Ii][Nn][Tt][Rr][Oo][Dd][Uu][Cc][Tt][Ii][Oo][Nn].*$"
+introPattern = "(^.*[Ii][Nn][Tt][Rr][Oo][Dd][Uu][Cc][Tt][Ii][Oo][Nn].*$)"
 introMatcher = re.compile(introPattern)
 concPattern = "^.*[C][Oo][Nn][Cc][Ll][Uu][Ss][Ii][Oo][Nn].*$"
 concMatcher = re.compile(concPattern)
-referencesPattern = "References"
+referencesPattern = ".*(References|REFERENCES)"
 referencesMatcher = re.compile(referencesPattern)
 emailPattern = "(^.*@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
 emailMatcher = re.compile(emailPattern)
 discussionPattern = "^.*[D][Ii][Ss][Cc][Uu][Ss][Ss][Ii][Oo][Nn].*$"
 discussionMatcher = re.compile(discussionPattern)
-acknowlegmentsPattern = "^.*[Aa][Cc][Kk][Nn][Oo][Ww][Ll][Ee][Dd][Gg][Ee][Mm][Ee][Nn][Tt][Ss].*$"
+acknowlegmentsPattern = "^.*[Aa][Cc][Kk][Nn][Oo][Ww][Ll][Ee][Dd][Gg]([Ee])?[Mm][Ee][Nn][Tt][Ss].*$"
 acknowlegmentsMatcher = re.compile(acknowlegmentsPattern)
-endIntroductionPattern = "((\s)*(2|II)(\.)?\s[A-Z](\w|\s|,)*$)|(^2(\s)?$)"
+endIntroductionPattern = "((\s)*(2|II)(\.)?\s[A-Z](\w|\s|,)*$)|(^2(\s)?$)|(^2.(\s)*$)"
 endIntroductionMatcher = re.compile(endIntroductionPattern)
 
 # --- Function for name recognition ---
@@ -91,97 +91,120 @@ def find_email(text):
                 res += " , " + splitedText[i] + " "
     return res
 
-def find_abstract(text):
+def find_abstract(text, ACTUAL_LINE):
     res = ""
     vf = True
     splitedText = text.splitlines()
+    CPT = 0
     for line in splitedText:
-        if vf and abstractMatcher.match(line):
-            vf = False
-        if (not vf) and (not introMatcher.match(line)):
-            res = res + line
-        if (not vf) and introMatcher.match(line):
-            break
-    if vf:
-        res = "No abstract was found"
-    return res
-
-def find_references(text):
-    res = ""
-    splitedText = text.splitlines()
-    vf = False
-    for line in splitedText:
-        if referencesMatcher.match(line):
-            vf = True
-        if vf:
-            res = res + line
-    if not vf:
-        res = "No references was found"
-    return res
-
-def find_discussion(text):
-    res = ""
-    vf = False
-    splitedText = text.splitlines()
-    for line in splitedText:
-        if vf:
-            if acknowlegmentsMatcher.match(line) or concMatcher.match(line):
-                break
-            else:
+        CPT += 1
+        if CPT >= ACTUAL_LINE:
+            if vf and abstractMatcher.match(line):
+                vf = False
+            if (not vf) and (not introMatcher.match(line)):
                 res = res + line
-        if not vf and discussionMatcher.match(line):
-            vf = True
+            if (not vf) and introMatcher.match(line):
+                ACTUAL_LINE = CPT
+                break
+    if vf:
+        return ["No abstract was found",-1]
+    return [res, ACTUAL_LINE]
+
+def find_references(text, ACTUAL_LINE):
+    res = ""
+    splitedText = text.splitlines()
+    vf = False
+    CPT = 0
+    for line in splitedText:
+        CPT += 1
+        if CPT >= ACTUAL_LINE:
+            if referencesMatcher.match(line):
+                vf = True
+            if vf:
+                res = res + line
     if not vf:
-        res = "No discussion was found"
-    return res;
+        return ["No references was found",-1]
+    return [res, 0]
+
+def find_discussion(text, ACTUAL_LINE):
+    res = ""
+    vf = False
+    splitedText = text.splitlines()
+    CPT = 0
+    for line in splitedText:
+        CPT += 1
+        if CPT >= ACTUAL_LINE:
+            if vf:
+                if acknowlegmentsMatcher.match(line) or concMatcher.match(line):
+                    ACTUAL_LINE = CPT
+                    break
+                else:
+                    res = res + line
+            if not vf and discussionMatcher.match(line):
+                vf = True
+    if not vf:
+        return ["No discussion was found",-1]
+    return [res, ACTUAL_LINE];
 
 
-def find_conclusion(text):
+def find_conclusion(text, ACTUAL_LINE):
     res=""
     splitedText = text.splitlines()
     vf = True
+    CPT = 0
     for line in splitedText:
-        if vf and concMatcher.match(line):
-            vf = False
-        if (not vf) and (not referencesMatcher.match(line) and not acknowlegmentsMatcher.match(line)):
-            res = res + line
-        if (not vf) and ( referencesMatcher.match(line) or acknowlegmentsMatcher.match(line) ):
-            break
+        CPT += 1
+        if CPT >= ACTUAL_LINE:
+            if vf and concMatcher.match(line):
+                vf = False
+            if (not vf) and (not referencesMatcher.match(line) and not acknowlegmentsMatcher.match(line)):
+                res = res + line
+            if (not vf) and ( referencesMatcher.match(line) or acknowlegmentsMatcher.match(line)):
+                ACTUAL_LINE = CPT
+                break
     if vf:
-        res = "No conclusion was found"
-    return res;
+        return ["No conclusion was found",-1]
+    return [res,ACTUAL_LINE];
 
-def find_introduction(text):
+def find_introduction(text,ACTUAL_LINE):
     res = ""
     splitedText = text.splitlines()
     vf = False
+    CPT = 0
     for line in splitedText:
-        if vf:
-            if endIntroductionMatcher.match(line):
-                break
-            else:
-                res = res + line
-        if not vf and introMatcher.match(line):
-            vf = True
+        CPT += 1
+        if CPT >= ACTUAL_LINE:
+            if vf:
+                if endIntroductionMatcher.match(line):
+                    ACTUAL_LINE = CPT
+                    break
+                else:
+                    res = res + line
+            if not vf and introMatcher.match(line):
+                vf = True
     if not vf:
-        res = "No introduction was found"
-    return res;
+        return ["No introduction was found",-1]
+    return [res,ACTUAL_LINE];
 
-def find_corp(text):
+def find_corp(text,ACTUAL_LINE):
     res = ""
     splitedText = text.splitlines()
     vf = False
+    CPT = 0
     for line in splitedText:
-        if vf:
-            if concMatcher.match(line) or discussionMatcher.match(line):
-                break
-            else:
-                res = res + line
-        if not vf and endIntroductionMatcher.match(line):
-            vf = True
+        CPT += 1
+        if CPT >= ACTUAL_LINE:
+            if vf:
+                if concMatcher.match(line) or discussionMatcher.match(line):
+                    ACTUAL_LINE = CPT
+                    break
+                else:
+                    res = res + line
+            if not vf and endIntroductionMatcher.match(line):
+                vf = True
     if not vf:
-        res = "No corp was found"
-    return res;
+        return ["No corp was found",-1]
+    return [res,ACTUAL_LINE];
 
 def find_affiliation(text,author):
     res=""
@@ -209,15 +232,30 @@ def find_affiliation(text,author):
     return affil;
 
 def write_xml(text,name):
+    ACTUAL_LINE = 0
+
     author = find_author(text)
     title = find_title(text, author)
-    abstract = find_abstract(text)
-    refs = find_references(text)
-    conclu = find_conclusion(text)
-    discussion = find_discussion(text)
-    introduction = find_introduction(text)
     affil = find_affiliation(text,author)
-    corp = find_corp(text)
+
+    author = find_author(text)
+    title = find_title(text, author)
+    abstract = find_abstract(text,ACTUAL_LINE)
+    if abstract[1] >= 0:
+        ACTUAL_LINE = abstract[1] - 2
+    introduction = find_introduction(text,ACTUAL_LINE)
+    if introduction[1] >= 0:
+        ACTUAL_LINE = introduction[1] - 2
+    corp = find_corp(text,ACTUAL_LINE)
+    if corp[1] >= 0:
+        ACTUAL_LINE = corp[1] - 2
+    discussion = find_discussion(text,ACTUAL_LINE)
+    if discussion[1] >= 0:
+        ACTUAL_LINE = discussion[1] - 2
+    conclu = find_conclusion(text,ACTUAL_LINE)
+    if conclu[1] >= 0:
+        ACTUAL_LINE = conclu[1] - 2
+    references = find_references(text,ACTUAL_LINE)
 
     article = etree.Element("article")
     preamble = etree.SubElement(article,"preamble")
@@ -238,42 +276,53 @@ def write_xml(text,name):
             affiliation.text = affil[0]
 
     abstractB = etree.SubElement(article,"abstract")
-    abstractB.text = abstract
+    abstractB.text = abstract[0]
     introductionB = etree.SubElement(article,"introduction")
-    introductionB.text = introduction
+    introductionB.text = introduction[0]
     corpB = etree.SubElement(article,"corp")
-    corpB.text = corp
+    corpB.text = corp[0]
     discussionB = etree.SubElement(article,"discussion")
-    discussionB.text = discussion
-    biblio = etree.SubElement(article,"biblio")
-    biblio.text = refs
+    discussionB.text = discussion[0]
     conclusion = etree.SubElement(article,"conclusion")
-    conclusion.text = conclu
+    conclusion.text = conclu[0]
+    biblio = etree.SubElement(article,"biblio")
+    biblio.text = references[0]
 
     file = open("res/" + name + ".xml","w+", encoding='utf-8')
     file.write((etree.tostring(article,pretty_print=True).decode("utf-8")))
     file.close()
 
 def write_text(text,name):
+    ACTUAL_LINE = 0
     author = find_author(text)
     title = find_title(text, author)
-    abstract = find_abstract(text)
-    references = find_references(text)
-    discussion = find_discussion(text)
-    introduction = find_introduction(text)
-    corp = find_corp(text)
-    conclu = find_conclusion(text)
+    abstract = find_abstract(text,ACTUAL_LINE)
+    if abstract[1] >= 0:
+        ACTUAL_LINE = abstract[1] - 2
+    introduction = find_introduction(text,ACTUAL_LINE)
+    if introduction[1] >= 0:
+        ACTUAL_LINE = introduction[1] - 2
+    corp = find_corp(text,ACTUAL_LINE)
+    if corp[1] >= 0:
+        ACTUAL_LINE = corp[1] - 2
+    discussion = find_discussion(text,ACTUAL_LINE)
+    if discussion[1] >= 0:
+        ACTUAL_LINE = discussion[1] - 2
+    conclu = find_conclusion(text,ACTUAL_LINE)
+    if conclu[1] >= 0:
+        ACTUAL_LINE = conclu[1] - 2
+    references = find_references(text,ACTUAL_LINE)
 
     file = open("res/" + name + ".txt","w+", encoding='utf-8')
     file.write("Nom du fichier: " + f + "\n\n")
     file.write("Titre: " + title + "\n\n")
     file.write("Auteurs: " + author + "\n\n")
-    file.write(abstract.replace("Abstract","Abstract : ") + "\n\n")
-    file.write("Introduction : "+introduction+"\n\n")
-    file.write("Corp : "+corp+"\n\n")
-    file.write("Discussion : "+discussion+"\n\n")
-    file.write("Conclusion : "+conclu+"\n\n")
-    file.write(references.replace("References","References : ") + "\n\n")
+    file.write(abstract[0].replace("Abstract","Abstract : ") + "\n\n")
+    file.write("Introduction : "+introduction[0]+"\n\n")
+    file.write("Corp : "+corp[0]+"\n\n")
+    file.write("Discussion : "+discussion[0]+"\n\n")
+    file.write("Conclusion : "+conclu[0]+"\n\n")
+    file.write(references[0].replace("References","References : ") + "\n\n")
     file.close()
 
 if __name__ == '__main__':
